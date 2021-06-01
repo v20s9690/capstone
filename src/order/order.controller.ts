e8d53b5130a875f15978ccec486b8a791ee843bb
lukka <v20s9690@gmail.com>
2021-06-02 01:57:12 +0900

    capstone

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -53,6 +53,7 @@ export class OrderController {
 
   @Post('getorder') // 받은 주문을 프론트(테이블)에게 배열 형태로 받아온다.
   async getOrder(@Req() request: Request): Promise<string> {
+    console.log(JSON.stringify(request.body));
     const reqOrder = JSON.parse(request.body["result"]);
     reqOrder.forEach((order: SimpleOrder) => {
       const orderEntity = new Order();
