5c7577950e29e1f1abb547799c1a2ce2c66541a6
lukka <v20s9690@gmail.com>
2021-06-04 03:30:34 +0900

    capstone

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -113,7 +113,7 @@ export class OrderController {
         const orders = await this.orderService.findByTableNo(tableNumber);
         for (const order of orders) {
           order.purchase = true;
-          order.save();
+          await order.save();
         }
         break;
     }
