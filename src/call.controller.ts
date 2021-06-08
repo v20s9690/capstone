d6dbbbac09ba939df96b05d1d2d5d07a693e3bd9
lukka <v20s9690@gmail.com>
2021-06-09 01:49:37 +0900

    capstone

---
+++ src/call.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+
+@Controller('call')
+export class CallController {
+  private tables: Set<string>;
+
+  @Get('table/:table_no')
+  async requestClerk(@Req() request: Request): Promise<string> {
+    let res = { result:[] };
+    const tableNumber = request.params.table_no;
+    this.tables.add(tableNumber);
+    res.result.push(tableNumber);
+    return JSON.stringify(res);
+  }
+
+  @Get('table/alarm')
+  async getRequests(@Req() request: Request): Promise<string> {
+    const tables = new Set();
+    this.tables.forEach(value => tables.add(value));
+    const result = JSON.stringify(tables);
+    this.tables.clear();
+    return JSON.stringify({result: result});
+  }
+}
